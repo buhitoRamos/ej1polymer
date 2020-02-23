3c1b77ae3a3718bd760a4b6bbab32453f687fc85
buhito85 <cym.martin85@gmail.com>
2020-02-23 02:46:36 -0300

    load user and view

---
+++ src/ej1-html-app/usuario.js
@@ -0,0 +1,16 @@
+class Usuario{
+    constructor( user, pass){
+   
+    this.user=user;
+    this.pass=pass;
+    this.nombre="";
+    this.apellido="";
+    this.dni="";
+    this.sexo="";
+    this.fecha ="";
+    this.localidad="";    
+    this.tratamiento="";
+}
+
+
+}
